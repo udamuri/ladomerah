24d952db6b03acd8fcbd8935e08f9794221d9f2a
muri.budiman <muri.budiman@redawning.com>
2016-10-12 16:33:07 +0700

    add nav header custome color

--- gallery_carousel/index.php
+++ gallery_carousel/index.php
@@ -10,6 +10,7 @@
     <!-- Bootstrap -->
     <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
     <link href="style.css" rel="stylesheet">
+    <link href="nav.css" rel="stylesheet">
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -19,10 +20,64 @@
     <![endif]-->
   </head>
   <body>
+    <nav class="navbar navbar-default">
+      <div class="container">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">L</a>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
+            <li><a href="#">Link</a></li>
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+              <ul class="dropdown-menu">
+                <li><a href="#">Action</a></li>
+                <li><a href="#">Another action</a></li>
+                <li><a href="#">Something else here</a></li>
+                <li role="separator" class="divider"></li>
+                <li><a href="#">Separated link</a></li>
+                <li role="separator" class="divider"></li>
+                <li><a href="#">One more separated link</a></li>
+              </ul>
+            </li>
+          </ul>
+          <form class="navbar-form navbar-left">
+            <div class="form-group">
+              <input type="text" class="form-control" placeholder="Search">
+            </div>
+            <button type="submit" class="btn btn-default">Submit</button>
+          </form>
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="#">Link</a></li>
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+              <ul class="dropdown-menu">
+                <li><a href="#">Action</a></li>
+                <li><a href="#">Another action</a></li>
+                <li><a href="#">Something else here</a></li>
+                <li role="separator" class="divider"></li>
+                <li><a href="#">Separated link</a></li>
+              </ul>
+            </li>
+          </ul>
+        </div><!-- /.navbar-collapse -->
+      </div><!-- /.container-fluid -->
+    </nav>
+
     <div class="container">
       <div class="row">
           <?php for($i = 0;$i<9;$i++) { ?>
-          <div class="col-md-4">
+          <div class="col-md-4 col-sm-6 col-xs-12">
               <div class="panel panel-default">
                 <div class="panel-heading">
                     <div id="carousel-lado<?php echo $i;?>" class="carousel slide carousel-slide" data-interval="false" data-ride="carousel">
@@ -39,7 +94,7 @@
                             <div class="small">
                               <img class="lazy" src="" data-src="img/2.jpg" alt="Second slide">
                               <div class="carousel-caption">
-                                    <p>First slide</p>
+                                    <p>Second slide</p>
                                 </div>
                             </div>
                           </div>
@@ -47,7 +102,7 @@
                             <div class="small">
                               <img class="lazy" src="" data-src="img/3.jpg" alt="Third slide">
                               <div class="carousel-caption">
-                                    <p>First slide</p>
+                                    <p>Third slide</p>
                                 </div>
                             </div>
                           </div>
@@ -64,9 +119,15 @@
                     </div>
                 </div>
                 <div class="panel-body">
-                  Panel content
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
                 </div>
-                <div class="panel-footer">Panel footer</div>
+                <div class="panel-footer">
+                      <button class="btn btn-default btn-sm">default</button>
+                      <button class="btn btn-primary btn-sm">primary</button>
+                      <button class="btn btn-warning btn-sm">warning</button>
+                      <button class="btn btn-danger btn-sm">danger</button>
+                      <button class="btn btn-info btn-sm">info</button>
+                </div>
               </div>
           </div>
 
