7f7796f41eb7ea72b50365b204d52e7b10369888
muri.budiman <muri.budiman@redawning.com>
2016-10-18 09:25:50 +0700

    add index design

--- gallery_carousel/index.php
+++ gallery_carousel/index.php
@@ -20,6 +20,9 @@
     <![endif]-->
   </head>
   <body>
+    <header class="container">
+        sdsd
+    </header>
     <nav class="navbar navbar-default">
       <div class="container">
         <!-- Brand and toggle get grouped for better mobile display -->
@@ -75,64 +78,7 @@
     </nav>
 
     <div class="container">
-      <div class="row">
-          <?php for($i = 0;$i<9;$i++) { ?>
-          <div class="col-md-4 col-sm-6 col-xs-12">
-              <div class="panel panel-default">
-                <div class="panel-heading">
-                    <div id="carousel-lado<?php echo $i;?>" class="carousel slide carousel-slide" data-interval="false" data-ride="carousel">
-                        <div class="carousel-inner" role="listbox">
-                          <div class="item active">
-                            <div class="small">
-                                <img class="lazy" src="img/1.jpg" alt="First slide" >
-                                <div class="carousel-caption">
-                                    <p>First slide</p>
-                                </div>
-                            </div>
-                          </div>
-                          <div class="item">
-                            <div class="small">
-                              <img class="lazy" src="" data-src="img/2.jpg" alt="Second slide">
-                              <div class="carousel-caption">
-                                    <p>Second slide</p>
-                                </div>
-                            </div>
-                          </div>
-                          <div class="item">
-                            <div class="small">
-                              <img class="lazy" src="" data-src="img/3.jpg" alt="Third slide">
-                              <div class="carousel-caption">
-                                    <p>Third slide</p>
-                                </div>
-                            </div>
-                          </div>
-                        </div>
-
-                        <a class="left carousel-control" href="#carousel-lado<?php echo $i;?>" role="button" data-slide="prev">
-                          <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-                          <span class="sr-only">Previous</span>
-                        </a>
-                        <a class="right carousel-control" href="#carousel-lado<?php echo $i;?>" role="button" data-slide="next">
-                          <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-                          <span class="sr-only">Next</span>
-                        </a>
-                    </div>
-                </div>
-                <div class="panel-body">
-                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-                </div>
-                <div class="panel-footer">
-                      <button class="btn btn-default btn-sm">default</button>
-                      <button class="btn btn-primary btn-sm">primary</button>
-                      <button class="btn btn-warning btn-sm">warning</button>
-                      <button class="btn btn-danger btn-sm">danger</button>
-                      <button class="btn btn-info btn-sm">info</button>
-                </div>
-              </div>
-          </div>
-
-          <?php } ?>
-      </div>
+        
     </div>
 
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
@@ -141,37 +87,7 @@
     <script src="bootstrap/js/bootstrap.min.js"></script>
 
     <script type="text/javascript">
-        var cHeight = 0;
-        $('.carousel-slide').on('slide.bs.carousel', function (e) {
-             var $nextImage = null;
-
-              $activeItem = $('.item.active', this);
-
-
-              if (e.direction == 'left'){
-                  $nextImage = $activeItem.next('.item').find('img');
-              } else {
-                  if ($activeItem.index() == 0){
-                      $nextImage = $('img:last', $activeItem.parent());
-                  } else {
-                      $nextImage = $activeItem.prev('.item').find('img');
-                  }
-              }
-
-              // prevents the slide decrease in height
-              if (cHeight == 0) {
-                 cHeight = $(this).height();
-                 $activeItem.next('.item').height(cHeight);
-              }
-
-              // prevents the loaded image if it is already loaded
-              var src = $nextImage.data('src');
-
-              if (typeof src !== "undefined" && src != "") {
-                 $nextImage.attr('src', src)
-                 $nextImage.data('src', '');
-              }
-        }) 
+       
     </script>
   </body>
 </html>
